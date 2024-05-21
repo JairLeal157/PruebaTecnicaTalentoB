da9d1eacfff296692ed63931bf3c17303e42fdfd
Jair Leal <jair.leal@udea.edu.co>
2024-05-21 17:08:54 -0500

    Initialize Spring Boot project

---
+++ src/main/java/com/prueba/peopleplace/PeoplePlaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.peopleplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplePlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplePlaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/peopleplace/PeoplePlaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.peopleplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplePlaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
